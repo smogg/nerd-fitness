c2e8d5854aa450782a07ff80a2eafaf15a95c76e
Oskar Zabik <oskar.zabik@gmail.com>
2017-11-21 21:06:44 +0100

    drift should only be a dev dep

--- project.clj
+++ project.clj
@@ -8,13 +8,13 @@
                  [org.clojure/java.jdbc "0.7.3"]
                  [org.postgresql/postgresql "42.1.4"]
                  [clojure.jdbc/clojure.jdbc-c3p0 "0.3.3"]
-                 [com.layerware/hugsql "0.4.8"]
-                 [drift "1.5.2"]]
+                 [com.layerware/hugsql "0.4.8"]]
   :plugins [[lein-ring "0.9.7"]
             [drift "1.5.2"]]
   :ring {:handler nerd-recipes.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]
+                        [ring/ring-mock "0.3.0"]
+                        [drift "1.5.2"]]
          :plugins [[lein-auto "0.1.3"]
                    [lein-cljfmt "0.5.7"]]}})
